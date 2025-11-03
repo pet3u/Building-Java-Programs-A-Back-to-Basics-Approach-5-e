01370997112c4210ca3babd48ec4ffc9c5196352
pet3u <petrub@uw.edu>
2025-11-03 15:17:57 -0800

    Add Solution for Exercise 1.10

---
+++ Building-Java-Programs-A-Back-to-Basics-Approach-5-e/Chapter1/BJP5_Exercise_1_10_Egg2.java
@@ -0,0 +1,43 @@
+public class Egg2 {
+    public static void main(String[] args){
+        eggTop();
+        eggBottom();
+        System.out.println("");
+        
+    }
+    
+    public static void eggTop(){
+        System.out.println("  _______  ");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+        
+        
+    }
+    public static void eggBottom(){
+        
+        System.out.println("\\         /");
+        System.out.println(" \\_______/");
+        System.out.println("-\"-'-\"-'-\"-");
+        
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
