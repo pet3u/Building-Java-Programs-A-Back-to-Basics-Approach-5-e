aba1223d7fd474a78b669c4908ee172b7e3a291f
pet3u <petrub@uw.edu>
2025-11-05 11:38:04 -0800

    Added Self-check 1_2 and Exercise 1_13

---
+++ Building-Java-Programs-A-Back-to-Basics-Approach-5-e/Chapter1/BJP5_Exercise_1_13_StarFigures.java
@@ -0,0 +1,30 @@
+public class StarFigures{
+   public static void main(String[] args){
+     doubleLine();
+   }
+
+   public static void doubleLine(){
+         System.out.println("*****");
+         System.out.println("*****");
+         System.out.println(" * * ");
+         System.out.println("  *  ");
+         System.out.println(" * * ");
+         System.out.println();
+         System.out.println("*****");
+         System.out.println("*****");
+         System.out.println(" * * ");
+         System.out.println("  *  ");
+         System.out.println(" * * ");
+         System.out.println("*****");
+         System.out.println("*****");
+         System.out.println();
+         System.out.println("  *  ");
+         System.out.println("  *  ");
+         System.out.println("  *  ");
+         System.out.println("*****");
+         System.out.println("*****");
+         System.out.println(" * * ");
+         System.out.println("  *  ");
+         System.out.println(" * * ");
+   }
+}
