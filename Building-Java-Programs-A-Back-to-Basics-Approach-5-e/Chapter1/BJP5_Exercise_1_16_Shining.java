7d47769b8e86f41dac64ce5579774364d5d5a4b5
pet3u <petrub@uw.edu>
2025-11-13 09:18:42 -0800

    Add Exercise 1.16 Chapter 1

---
+++ Building-Java-Programs-A-Back-to-Basics-Approach-5-e/Chapter1/BJP5_Exercise_1_16_Shining.java
@@ -0,0 +1,63 @@
+public class Shining{
+    public static void main(String[] args){
+    
+          thirdTen();
+    }
+    
+    public static void firstTen() {
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+        System.out.println("All work and no play makes Jack a dull boy.");
+    }
+    
+    public static void secondTen() {
+        firstTen();
+        firstTen();
+        firstTen();
+        firstTen();
+        firstTen();
+        firstTen();
+        firstTen();
+        firstTen();
+        firstTen();
+        firstTen();
+    }
+    
+    public static void thirdTen() {
+        secondTen();
+        secondTen();
+        secondTen();
+        secondTen();
+        secondTen();
+        secondTen();
+        secondTen();
+        secondTen();
+        secondTen();
+        secondTen();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
