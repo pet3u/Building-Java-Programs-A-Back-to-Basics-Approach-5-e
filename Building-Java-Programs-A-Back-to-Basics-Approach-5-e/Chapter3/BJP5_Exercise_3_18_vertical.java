0c897276bcbe3002dbbd398cec60b3c916663712
pet3u <petrub@uw.edu>
2025-11-16 18:05:31 -0800

    Add Exercise 3.18 Chapter 3

---
+++ Building-Java-Programs-A-Back-to-Basics-Approach-5-e/Chapter3/BJP5_Exercise_3_18_vertical.java
@@ -0,0 +1,15 @@
+public class BJP5_Exercise_3_18_vertical{
+   
+   public static void main(String[] args){
+      vertical("Hey now");
+   }
+   
+   public static void vertical(String word){
+         
+      for(int i = 0; i < word.length(); i++){
+         System.out.println(word.charAt(i));
+      }
+      
+   }
+   
+}
