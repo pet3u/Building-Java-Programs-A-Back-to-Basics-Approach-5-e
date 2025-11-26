c77ba6b9fb604359a3b0b015770db2b058a2310e
pet3u <petrub@uw.edu>
2025-11-25 22:46:13 -0800

    Add BJP5_Exercise_2_7_spacedNumbers

---
+++ Building-Java-Programs-A-Back-to-Basics-Approach-5-e/Chapter2/BJP5_Exercise_2_7_spacedNumbers.java
@@ -0,0 +1,9 @@
+for (int i = 1; i <= 5; ++i){
+      
+         for( int j = 4; j >= i; --j){
+            
+            System.out.print(" ");
+         
+         }
+         System.out.println(i);
+      }
