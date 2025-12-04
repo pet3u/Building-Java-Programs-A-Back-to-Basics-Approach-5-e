62c1ddde1bac50432b73e438eab061c4baeec1f5
pet3u <petrub@uw.edu>
2025-12-03 20:49:41 -0800

    Add Exercise BJP5_Exercise_1_17_FarewellGoodBye Chapter 1

---
+++ Building-Java-Programs-A-Back-to-Basics-Approach-5-e/Chapter1/BJP5_Exercise_1_17_FarewellGoodBye.java
@@ -0,0 +1,26 @@
+public class FarewellGoodBye{
+    public static void main(String[] args){
+        One();
+        System.out.println("good night!");
+        System.out.println("It's time, to go,");
+        System.out.println("and I'll be out of sight!");
+        System.out.println();
+        One();
+        System.out.println("take care!");
+        System.out.println("I'll say, goodbye,");
+        System.out.println("that's neither here nor there!");
+        System.out.println();
+        One();
+        System.out.println("see you later!");
+        System.out.println("I hope, you think,");
+        System.out.println("I'm a lover, not a hater!");
+        System.out.println();
+    
+    }
+    
+    public static void One(){
+        System.out.println("Farewell,");
+        System.out.println("goodbye,");
+        System.out.println("au revoir,");
+    }
+}
