81e81414e2aa2e24f8e0bce9e2efaf2533ab33c7
pet3u <petrub@uw.edu>
2025-12-05 09:05:43 -0800

    Add BJP5_Exercise_1_9_Egg Chapter 1

---
+++ Building-Java-Programs-A-Back-to-Basics-Approach-5-e/Chapter1/BJP5_Exercise_1_9_Egg.java
@@ -0,0 +1,18 @@
+public class Egg {
+    public static void main(String[] args){
+        lid();
+        System.out.println("-\"-'-\"-'-\"-");
+        cup();
+  
+    }
+        public static void cup(){
+        System.out.println("\\         /");
+        System.out.println(" \\_______/"); 
+    }
+    
+        public static void lid(){
+        System.out.println("  _______  ");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+    }
+}
