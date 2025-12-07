7211db378bca9915c6f2fcf8a3afe1716618e746
pet3u <petrub@uw.edu>
2025-12-06 22:15:58 -0800

    Add BJP5_Exercise_2_9_waveNumbers40 Chapter 2

---
+++ Building-Java-Programs-A-Back-to-Basics-Approach-5-e/Chapter2/BJP5_Exercise_2_9_waveNumbers40.java
@@ -0,0 +1,27 @@
+for(int i = 0; i < 40; i++){
+    System.out.print("-");
+    
+}
+    System.out.println();
+for(int i = 0; i < 10; i++){
+    System.out.print("_-^-");
+    
+}
+    System.out.println();
+for (int j = 0; j < 2; j++){
+    
+    for(int i = 1; i < 10; i++){
+        System.out.print(i);
+        System.out.print(i);
+    }
+    System.out.print(0);
+    System.out.print(0);
+    
+}    
+
+System.out.println();
+
+for(int i = 0; i < 40; i++){
+    System.out.print("-");
+    
+}
