d10a6d067c77031e8602723a29d984e187a88d1a
pet3u <petrub@uw.edu>
2025-12-07 19:40:09 -0800

    Add BJP5_Exercise_2_13_nestedNumbers2 Chapter 2

---
+++ Building-Java-Programs-A-Back-to-Basics-Approach-5-e/Chapter2/BJP5_Exercise_2_13_nestedNumbers2.java
@@ -0,0 +1,10 @@
+final int repetition = 5;
+
+for(int i = 0; i < repetition; i++){
+    for(int j = 9; j >= 0; j--){
+        for(int k = 0; k < repetition; k++){
+            System.out.print(j);
+        } 
+    }
+    System.out.println();
+}
