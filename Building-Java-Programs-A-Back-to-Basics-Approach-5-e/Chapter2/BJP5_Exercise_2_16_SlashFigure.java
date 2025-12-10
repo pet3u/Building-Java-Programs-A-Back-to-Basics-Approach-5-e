d98def0130662f83dda26c9c16cfd792d3a01a6a
pet3u <petrub@uw.edu>
2025-12-10 15:52:35 -0800

    Add BJP5_Exercise_2_16_SlashFigure Chapter 2

---
+++ Building-Java-Programs-A-Back-to-Basics-Approach-5-e/Chapter2/BJP5_Exercise_2_16_SlashFigure.java
@@ -0,0 +1,44 @@
+public class SlashFigure{
+
+   public static final int SIZE = 6;
+    
+    public static void main(String[] args){
+    
+        for(int i = 1; i <= SIZE; i++){
+
+            for(int j = 1; j <= 2 * i - 2; j++){
+
+                System.out.print("\\");
+
+            }
+            
+            for(int j = 1; j <= (-4 * i + (4 * SIZE + 2)); j++){
+
+                System.out.print("!");
+
+            }
+            
+            for(int j = 1; j <= 2 * i - 2; j++){
+
+                System.out.print("/");
+
+            }
+            
+            System.out.println();
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
